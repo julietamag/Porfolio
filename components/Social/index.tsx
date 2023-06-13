c180e88129f35516b31f027f89deb30595aef7f7
Julieta Magnago <julietamagnago@gmail.com>
2023-06-13 15:59:30 +0200

    fix social links

--- components/Social/index.tsx
+++ components/Social/index.tsx
@@ -15,32 +15,37 @@ const Social = (props: Props) => {
 
   return (
     <>
-      {windowSize > 650 && (
-          <div className="xl:fixed xl:bottom-0 xl:ml-5 xl:z-20 ">
-        <ul className="flex lg:flex-col gap-4 items-center justify-center  ">
-          <li>
-            <Link href={"https://github.com/julietamag"} target="blank">
-              <GitHub />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"https://www.linkedin.com/in/julieta-magnago/"}
-              target="blank"
-            >
-              <LinkedIn />
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://www.instagram.com/julimag__/"} target="blank">
-              <Instagram />
-            </Link>
-          </li>
-          <li className="border border-indigo-900 h-16"></li>
-        </ul>
+      {windowSize > 950 ? (
+        <div className="xl:fixed xl:bottom-0 xl:ml-5 xl:z-20 ">
+          <ul className="flex lg:flex-col gap-4 items-center justify-center  ">
+            <li>
+              <Link href={"https://github.com/julietamag"} target="blank">
+                <GitHub />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"https://www.linkedin.com/in/julieta-magnago/"}
+                target="blank"
+              >
+                <LinkedIn />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"https://www.instagram.com/julimag__/"}
+                target="blank"
+              >
+                <Instagram />
+              </Link>
+            </li>
+            <li className="border border-indigo-900 h-16"></li>
+          </ul>
         </div>
+      ) : (
+        ""
       )}
-      </>
+    </>
   );
 };
 
